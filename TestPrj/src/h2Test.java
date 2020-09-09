09af277423b27ce9623c90e1ef71cd72f68da4e4
leehyunhwa <hyunhwa_lee@tmax.co.kr>
2020-09-09 09:17:46 +0000

    test

--- TestPrj/src/h2Test.java
+++ TestPrj/src/h2Test.java
@@ -4,7 +4,8 @@ public class h2Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("TEST");
-		System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("ATEST");
 	}
 
 }
